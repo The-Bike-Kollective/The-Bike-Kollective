6598451f2759e4eb89f5197bd764dcd22715339c
ali-jal <52464443+ali-jal@users.noreply.github.com>
2022-04-18 20:22:25 -0700

    User class and interface is added

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,23 +1,56 @@
-// class User {
-//     id: number;
-//     family_name: string;
-//     given_name: string;
-//     birth_date: string;
-//     email: string;
-//     identifier: string;
-//     owned_biks: Array<number>;
-//     check_out_bike: number;
-//     checked_out_time: number;
-//     suspended: boolean;
+interface IUser {
+  id?: number;
+  family_name: string;
+  given_name: string;
+  email: string;
+  identifier: string;
+  owned_biks: Array<number>;
+  check_out_bike: number;
+  checked_out_time: number;
+  suspended: boolean;
+  access_token: string;
+  refresh_token: string;
+}
+
+class User {
+    id?: number;
+    family_name: string;
+    given_name: string;
+    email: string;
+    identifier: string;
+    owned_biks: Array<number>;
+    checked_out_bike: number;
+    checked_out_time: number;
+    suspended: boolean;
+    access_token: string;
+    refresh_token: string;
     
+  constructor(
+    family_name: string,
+    given_name: string,
+    email: string,
+    identifier: string,
+    owned_biks: Array<number>,
+    checked_out_bike: number,
+    checked_out_time: number,
+    suspended: boolean,
+    access_token: string,
+    refresh_token: string,
+    id?: number | undefined,
+  ) {
+      this.id = id;
+      this.family_name = family_name;
+      this.given_name= given_name;
+      this.email=email;
+      this.identifier= identifier;
+      this.owned_biks=owned_biks;
+      this.checked_out_bike= checked_out_bike;
+      this.checked_out_time= checked_out_time;
+      this.suspended=suspended;
+      this.access_token=access_token;
+      this.refresh_token=refresh_token;
+  }
+    
+}
 
-
-   
-//     constructor(message: string) {
-//       this.greeting = message;
-//     }
-   
-//     greet() {
-//       return "Hello, " + this.greeting;
-//     }
-//   }
+export {IUser , User}
