b3695e28772771c57252b6ef7b2c1981f12a816c
ali-jal <52464443+ali-jal@users.noreply.github.com>
2022-04-24 15:17:28 -0700

    bike db model and schemca added

--- api/src/db/db.ts
+++ api/src/db/db.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import {IUser} from '../models/user';
+import {IBike} from '../models/bike';
 
 const connectDB = async function () {
   await mongoose.connect(
@@ -25,7 +26,25 @@ const UserSchema: mongoose.Schema = new mongoose.Schema({
 
 });
 
+const BikeSchema: mongoose.Schema = new mongoose.Schema({
+  date_added: { type: Number, required: true },
+  image: { type: String, required: true },
+  active: { type: Boolean, required: true },
+  condition: { type: Boolean, required: true },
+  owner_id: { type: String, required: true },
+  lock_combination: { type: Number, required: true },
+  notes: { type: [Object], required: true },
+  rating: { type: Number, required: true },
+  rating_history: { type: [Object], required: true },
+  location_long: { type: Number, required: true },
+  location_lat: { type: Number, required: true },
+  check_out_id: { type: String, required: true },
+  check_out_history: { type: [Object], required: true }
+});
+
 const User: mongoose.Model<IUser> = mongoose.model('User', UserSchema);
+const Bike: mongoose.Model<IBike> = mongoose.model('Bike', BikeSchema);
+
 
 const addUsertoDB = async(newUser:IUser)=>{
   const result = await User.create(newUser);
@@ -33,5 +52,14 @@ const addUsertoDB = async(newUser:IUser)=>{
   console.log(result);
   return result;
 }
+
+const addBiketoDB = async(newBike:IBike)=>{
+  const result = await Bike.create(newBike);
+  console.log('ADED!');
+  console.log(result);
+  return result;
+}
+
+
 
 export {connectDB,addUsertoDB , IUser};
