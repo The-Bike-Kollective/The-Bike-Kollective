065c49625cf53bc587c848d32c17095a3a82c638
ali-jal <52464443+ali-jal@users.noreply.github.com>
2022-05-22 19:03:13 -0700

    fixed POST /images bugs

--- api/src/routes/imageRoutes.ts
+++ api/src/routes/imageRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { uploadImage } from "../services/firebase";
-import {verifyUserIdentity,userRegistration} from './userHelperFunctions'
+import {verifyUserIdentity} from './userHelperFunctions'
 
 import {findUserByIdentifier,findUserByAccessToekn} from "../db/db";
 
@@ -11,12 +11,16 @@ const router = express.Router();
 // @params: none . (base64 image in JSON body)
 // @return: direct download link as string in JSON
 // bugs: no known bugs
-// TODO: error handling
 router.post("/", async (req, res) => {
 
   // check if access_token is provided.
   if (!req.headers.authorization) {
     return res.status(403).json({ message: "access token is missing" });
+  }
+
+  // verify body
+  if (!(await verifyUploadImagePostBody(req.body))) {
+    return res.status(400).send({ message: "invalid body"});
   }
 
   // splits "Breaer TOKEN" 
@@ -30,7 +34,7 @@ router.post("/", async (req, res) => {
   const verificationResult = await verifyUserIdentity(userFromDb,access_token)
 
   if (verificationResult==404){
-    return res.status(404).json({ message: "User not found" , access_token: access_token});
+    return res.status(404).json({ message: "User not found. non existing access token" , access_token: access_token});
   }else if (verificationResult==500){
     return res.status(500).json({ message: "Multiple USER ERROR" , access_token: access_token});
   }else if (verificationResult==401){
@@ -52,7 +56,29 @@ router.post("/", async (req, res) => {
 
   } 
   
-  
 });
+
+// information/instructions: verifies body data for image POST
+// @params: JSON object form req body
+// @return: true if valid, flase if not
+// bugs: no known bugs
+const verifyUploadImagePostBody = (body: object) => {
+  return new Promise(async (resolve) => {
+    const valid_keys = ["image"];
+    const keys_in_body = Object.keys(body);
+
+    if (valid_keys.length != keys_in_body.length) {
+      return resolve(false);
+    }
+
+    keys_in_body.forEach((key) => {
+      if (!valid_keys.includes(key)) {
+        return resolve(false);
+      }
+    });
+
+    return resolve(true);
+  });
+};
 
 module.exports = router;
